91231d5b4c28275168a6d1b8a72361d1ae61583c
preethiediga <30634374+preethiediga@users.noreply.github.com>
2017-08-01 23:41:48 +0530

    Initial_Commit

---
+++ IR_Sensor.ino
@@ -0,0 +1,22 @@
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2,INPUT);
+  
+  Serial.begin(9600);
+  Serial.print("Preethi here, The sensor values\n");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(2)== 0)
+  {
+    Serial.println("white");
+  }
+   else
+  {
+    Serial.println("black");
+  }
+  delay(300);
+}

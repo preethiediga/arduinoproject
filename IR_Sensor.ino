0ea390edb7db0050694f9329dc3021f421315dfd
preethiediga <30634374+preethiediga@users.noreply.github.com>
2017-08-01 23:45:47 +0530

    Comments updated

--- IR_Sensor.ino
+++ IR_Sensor.ino
@@ -2,7 +2,7 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(2,INPUT);
-  
+  // Initialise serial port with baud rate 9600
   Serial.begin(9600);
   Serial.print("Preethi here, The sensor values\n");
 
